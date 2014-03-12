d9aaf1ccb7bf43d76ebfe6fa751dae7d7dcf22f9
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-03-12 08:20:04 +0100

    update scripts

---
+++ voicecontrol2/alllights_on.sh
@@ -0,0 +1,1 @@
+RaspberryPi/433mhz/send 5510485 && RaspberryPi/433mhz/send 5525845 && tts 'All lights on!' 2>/dev/null && curl --data-binary '{ "jsonrpc": "2.0", "method": "GUI.ShowNotification", "params":{"title":"J.A.R.V.I.S.","message":"All lights on!"},"id":1 }' -H 'content-type: application/json;' http://balabis:balabis1234@127.0.0.1:9453/jsonrpc

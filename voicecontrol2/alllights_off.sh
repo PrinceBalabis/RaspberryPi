f52a31f7709c8035a3dee9ca2be1118efb5ce985
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-03-12 10:19:13 +0100

    alllight bug fix

--- voicecontrol2/alllights_off.sh
+++ voicecontrol2/alllights_off.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/root/RaspberryPi/433mhz/send 5525844 && RaspberryPi/433mhz/send 5510484 && tts 'All lights off!' 2>/dev/null && curl --data-binary '{ "jsonrpc": "2.0", "method": "GUI.ShowNotification", "params":{"title":"J.A.R.V.I.S.","message":"All lights off!"},"id":1 }' -H 'content-type: application/json;' http://balabis:balabis1234@127.0.0.1:9453/jsonrpc 
+/root/RaspberryPi/433mhz/send 5525844 && /root/RaspberryPi/433mhz/send 5510484 && tts 'All lights off!' 2>/dev/null && curl --data-binary '{ "jsonrpc": "2.0", "method": "GUI.ShowNotification", "params":{"title":"J.A.R.V.I.S.","message":"All lights off!"},"id":1 }' -H 'content-type: application/json;' http://balabis:balabis1234@127.0.0.1:9453/jsonrpc 
